8eb4f75912d96eb0463ece218a32db3dffa85d76
BKChia <119401127+BKChia@users.noreply.github.com>
2023-03-30 12:51:13 +0800

    Update q2.js

--- q2.js
+++ q2.js
@@ -1,23 +1,24 @@
 /*
     Task 1: 
     - Declare an array that are going to be used to store patient's name.
-
     Task 2: 
     - Add code to add patient's name into the array declared in task 1.
-
     Task 3: 
     - Implement listPatient() function to print all patient's name stored in the array
 */
 
 
-// Task 1: Add code here
 
+const listpatient = new Array (3);
+let patientname;
+var x=0
 function addPatient(patientName){
-    // Task 2: Add code here
+    listpatient[x]=patientName;
+    x=x+1
 }
-
-// Task 3: Add code here
-
+function listPatient(){
+    println (listpatient);
+}
 
 addPatient("John");
 addPatient("Mary");
